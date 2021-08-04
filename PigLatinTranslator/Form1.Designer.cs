d2b2623f58246c81df33fd6c107c4008240bf7ca
Sheldon Codling <shelcod@gmail.com>
2021-08-04 03:06:08 -0400

    Add files via upload

---
+++ PigLatinTranslator/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace PigLatinTranslator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.englishTxt = new System.Windows.Forms.TextBox();
+            this.translateBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pigTxt = new System.Windows.Forms.TextBox();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // englishTxt
+            // 
+            this.englishTxt.Location = new System.Drawing.Point(12, 25);
+            this.englishTxt.Multiline = true;
+            this.englishTxt.Name = "englishTxt";
+            this.englishTxt.Size = new System.Drawing.Size(403, 229);
+            this.englishTxt.TabIndex = 0;
+            // 
+            // translateBtn
+            // 
+            this.translateBtn.Location = new System.Drawing.Point(176, 260);
+            this.translateBtn.Name = "translateBtn";
+            this.translateBtn.Size = new System.Drawing.Size(75, 23);
+            this.translateBtn.TabIndex = 2;
+            this.translateBtn.Text = "Translate";
+            this.translateBtn.UseVisualStyleBackColor = true;
+            this.translateBtn.Click += new System.EventHandler(this.translateBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Your Message:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Translated Message:";
+            // 
+            // pigTxt
+            // 
+            this.pigTxt.Location = new System.Drawing.Point(12, 289);
+            this.pigTxt.Multiline = true;
+            this.pigTxt.Name = "pigTxt";
+            this.pigTxt.Size = new System.Drawing.Size(403, 229);
+            this.pigTxt.TabIndex = 1;
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(340, 524);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 2;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 558);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.translateBtn);
+            this.Controls.Add(this.pigTxt);
+            this.Controls.Add(this.englishTxt);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Pig Latin Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox englishTxt;
+        private System.Windows.Forms.Button translateBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox pigTxt;
+        private System.Windows.Forms.Button clearBtn;
+    }
+}
+
